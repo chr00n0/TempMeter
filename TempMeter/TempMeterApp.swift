ecbe371606e55e04abe05551ee1fcabde48683e4
Emil Piórkowski <e.piorkowski11@gmail.com>
2026-03-14 19:43:52 +0100

    Initial commit

---
+++ TempMeter/TempMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempMeterApp.swift
+//  TempMeter
+//
+//  Created by Emil Piórkowski on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TempMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
